adf3524cc0d1dbeccafa5271665330423ed1685c
Artur Rosa <dev@arturrosa.pl>
2023-04-02 22:51:34 +0200

    fix: use-selection command waits for modifiers release before performing cmd+c

--- assets/copy-to-clipboard.scpt
+++ assets/copy-to-clipboard.scpt
@@ -1,3 +1,20 @@
+use framework "Cocoa"
+use scripting additions
+
+global ca
+set ca to current application
+
+to isModifierPressed(modifier)
+    ((ca's NSEvent's modifierFlags()) / modifier as integer) mod 2 is equal to 1
+end isModifierPressed
+
+-- wait until all modifiers except "cmd" are released
+-- it's important if the script is executed with a shortcut
+-- for "cmd + c" to have a desired effect
+repeat while isModifierPressed(ca's NSEventModifierFlagShift) or isModifierPressed(ca's NSEventModifierFlagControl) or isModifierPressed(ca's NSEventModifierFlagOption)
+    delay 0.1
+end repeat
+
 tell application "System Events"
   keystroke "c" using command down
 end tell
--- src/use-selection.ts
+++ src/use-selection.ts
@@ -1,14 +1,13 @@
-import { Clipboard, launchCommand, LaunchType } from '@raycast/api';
-import { execSync } from 'child_process';
+import { Clipboard, launchCommand, LaunchType } from "@raycast/api";
+import { execSync } from "child_process";
 
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 export default async () => {
   const previousClipboard = await Clipboard.read();
-  const scriptPath = __dirname + '/assets/copy-to-clipboard.scpt';
-  await delay(100);
+  const scriptPath = __dirname + "/assets/copy-to-clipboard.scpt";
   execSync(`osascript ${scriptPath}`);
   await delay(100);
   const input = await Clipboard.readText();
   await Clipboard.copy(previousClipboard);
-  launchCommand({ name: 'command-picker', type: LaunchType.UserInitiated, context: { input } });
-}
+  launchCommand({ name: "command-picker", type: LaunchType.UserInitiated, context: { input } });
+};
